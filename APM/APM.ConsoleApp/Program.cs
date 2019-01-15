6de93b234a88a76251a2b7ae8fd370794d912722
Manuel <manuel.diaz@svcfin.com>
2019-01-15 12:09:28 -0500

    Added console app for testing purpouses

---
+++ APM/APM.ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace APM.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/xml"));
+
+            var result = client.GetAsync(new Uri("http://localhost:61452/api/products")).Result;
+            if(result.StatusCode== HttpStatusCode.OK)
+            {
+                var doc = XDocument.Load(result.Content.ReadAsStreamAsync().Result);
+                var ns = (XNamespace)"http://schemas.datacontract.org/2004/07/APM.WebAPI.Models";                
+
+                foreach (var name in doc.Descendants(ns + "ProductName"))
+                {
+                    Console.WriteLine(name.Value);
+                }
+            }
+        }
+    }
+}
